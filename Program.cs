ca9d7ecd22b9d60fabe85c092f9b71cfce5e5000
OseiEsther <oseiamoakoesther2022@gmail.com>
2025-08-09 13:47:51 +0000

    first task

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OPPConcepts
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance & Method Overriding");
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
+
+
+
